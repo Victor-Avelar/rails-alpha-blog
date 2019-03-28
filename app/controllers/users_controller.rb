d2df3bf5f8ea5a486145edcb94ad2e8256b45a75
Victor-Avelar <victor.r.avelar@gmail.com>
2019-03-28 14:31:34 -0700

    Add users listing and update show article view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,14 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
